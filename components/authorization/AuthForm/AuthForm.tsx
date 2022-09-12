015dd3a06e63f62d85b4920871c2e14f7427819f
Dawid <daw.tomczyk1997@gmail.com>
2022-09-12 20:18:54 +0200

    formik stepper component

--- components/authorization/AuthForm/AuthForm.tsx
+++ components/authorization/AuthForm/AuthForm.tsx
@@ -1,39 +1,108 @@
-import { Field, Form, Formik, useFormik } from "formik";
-import React from "react";
+import {
+  Field,
+  Form,
+  Formik,
+  FormikConfig,
+  FormikValues,
+  useFormik,
+} from "formik";
+import React, { useState } from "react";
+import { basicSchema } from "../../../schemas";
 import { Heading } from "../../atoms/Heading";
 import * as Styled from "./styles";
 
 interface Props {
   isRegister: boolean;
+  onSubmit: any;
 }
 
-const AuthForm = ({ isRegister }: Props) => {
-  const {values,handleChange,handleBlur} = useFormik({
-    initialValues:{
-      email:"",
-      age:"",
-      password:"",
-      confirmPassword:""
-    },
-  });
+interface FormValues {
+  email: string;
+  age: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const TextField = () => <input />;
+const Checkbox = () => <input type="checkbox" />;
+
+const initialValues = {
+  firstname: "",
+  lastname: "",
+  millionaire: false,
+  money: 1000,
+  description: "",
+};
+
+const AuthForm = ({ isRegister, onSubmit }: Props) => {
   return (
     <Styled.Wrapper>
       <Heading isLarge level="h1">
         {isRegister ? "Sign up" : "Sign in"}
       </Heading>
       <div>
-        <form autoComplete="off">
-            <input id="email" value={values.email} onChange={handleChange} placeholder="Enter your email" />
-            <input id="age" value={values.age} onChange={handleChange} placeholder="Enter your age" />
-            <input id="password" value={values.password} onChange={handleChange} placeholder="Enter your password" />
-        </form>
+        <FormikStepper initialValues={initialValues} onSubmit={() => {}}>
+          <FormikStep >
+            <Field name="firstname" label="First Name" />
+            <Field name="lastname" label="Last Name" />
+            <Field
+              name="millionaire"
+              type="checkbox"
+              label="Im a millionaire"
+            />
+          </FormikStep>
+          <FormikStep >
+            <Field name="money" type="number" label="Money" />
+          </FormikStep>
 
-                
-
-     
+          <FormikStep >
+            <Field name="description" label="Description" />
+          </FormikStep>
+        </FormikStepper>
       </div>
     </Styled.Wrapper>
   );
 };
 
 export default AuthForm;
+
+interface FormikStepProps
+  extends Pick<FormikConfig<FormikValues>, "children" | "validationSchema"> {}
+
+export const FormikStep = ({ children }: FormikStepProps) => {
+  return <>{children}</>;
+};
+
+export const FormikStepper = ({
+  children,
+  ...props
+}: FormikConfig<FormValues> | any) => {
+  const childrenArray = React.Children.toArray(children) as React.ReactElement<FormikStepProps>[];
+  const [step, setStep] = useState(0);
+  const currentChild = childrenArray[step];
+  console.log(currentChild.props)
+
+  const isLastStep = () => step === childrenArray.length - 1;
+
+  return (
+    <Formik
+      {...props}
+      validationSchema={currentChild.props.validationSchema}
+      onSubmit={async (values: any, helpers: any) => {
+        if (step === childrenArray.length - 1) {
+          await props.onSubmit(values, helpers);
+        } else {
+          setStep((prev) => prev + 1);
+        }
+      }}
+    >
+      <Form autoComplete="off">
+        {currentChild}
+        {step > 0 ? (
+          <button onClick={() => setStep((prev) => prev - 1)}>Back</button>
+        ) : null}
+        <button type="submit">{isLastStep() ? "Submit" : "Next"}</button>
+      </Form>
+    </Formik>
+  );
+};
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,19 @@
-import * as yup from 'yup';
+import {string,number,mixed,object,ref} from 'yup'
 
-const basicSchema = yup.object().shape({
-    email:yup.string().email("Please enter a valid email"),
-    age: yup.number()
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+// min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+
+export const basicSchema = object().shape({
+    // email:string().email("Please enter a valid email").required("Required"),
+    // age: number().positive().integer("Age must be a number").required("Required"),
+    // password: string().min(5).matches(passwordRules, {message:"Please create a stronger password"}).required("Required"),
+    // confirmPassword: string().oneOf([ref('password'), null]),
+
+
+    money:mixed().when("millionaire",{
+        is:true,
+        then: number().required("This field is required").min(1_000_000, "You need 1 million"),
+        otherwise: number().required("This field is required")
+    })
 })
